6b147f25acf2fcfbef0223cf351bc0e1d36528a9
seeks-imac <seeksdream@qq.com>
2023-02-23 10:21:31 +0800

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <template>
-  <div class="play-container">
-    <div style="padding: 10px; background-color: #999999; color: #ffffff">
-      Relation graph:
-    </div>
-    <div
-      style="
-        border: #efefef solid 1px;
-        height: calc(100vh - 100px);
-        width: 100%;
-      "
-    >
-      <relation-graph ref="relationGraph$" :options="options" />
+  <div>
+    <div style="border: #efefef solid 1px; height: calc(100vh - 100px);width: 100%;">
+      <relation-graph ref="relationGraph$" :options="options">
+        <template #node="{node}">
+          <button>{{node.text}}</button>
+        </template>
+      </relation-graph>
     </div>
   </div>
 </template>
@@ -18,11 +13,11 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import RelationGraph from 'relation-graph/vue3'
-// code here
-console.log('------------App---------', RelationGraph)
-const relationGraph$ = ref<RelationGraph>()
+import RelationGraphElement from "relation-graph";
+const relationGraph$ = ref<RelationGraphElement>()
 const options = {
   defaultExpandHolderPosition: 'right',
+  defaultLineShape: 4,
   debug: true,
   showDebugPanel: true,
 }
@@ -30,68 +25,21 @@ onMounted(() => {
   const jsonData = {
     rootId: 'a',
     nodes: [
-      {
-        id: 'a',
-        text: 'a',
-      },
-      {
-        id: 'b',
-        text: 'b',
-      },
-      {
-        id: 'c',
-        text: 'c',
-      },
-      {
-        id: 'd',
-        text: 'd',
-      },
-      {
-        id: 'e',
-        text: 'e',
-      },
-      {
-        id: 'f',
-        text: 'f',
-      },
+      { id: 'a', text: 'a', },
+      { id: 'b', text: 'b', },
+      { id: 'c', text: 'c', },
+      { id: 'd', text: 'd', },
+      { id: 'e', text: 'e', },
+      { id: 'f', text: 'f', },
     ],
     lines: [
-      {
-        from: 'a',
-        to: 'b',
-      },
-      {
-        from: 'a',
-        to: 'c',
-      },
-      {
-        from: 'a',
-        to: 'd',
-      },
-      {
-        from: 'a',
-        to: 'e',
-      },
-      {
-        from: 'a',
-        to: 'f',
-      },
+      { from: 'a', to: 'b', },
+      { from: 'a', to: 'c', },
+      { from: 'a', to: 'd', },
+      { from: 'a', to: 'e', },
+      { from: 'a', to: 'f', },
     ],
   }
   relationGraph$.value.setJsonData(jsonData)
 })
 </script>
-
-<style>
-html,
-body {
-  width: 100vw;
-  height: 100vh;
-  margin: 0px;
-  padding: 0px;
-}
-.play-container {
-  height: calc(100% - 100px);
-  width: calc(100% - 100px);
-}
-</style>
